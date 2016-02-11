3d755c65b3df7522b8a04e20394ea8e6e86762df
Nikos Yotis <nyotis@users.noreply.github.com>
2016-02-11 17:07:53 +0100

    update build for a modern Linux distribution

--- project.sh
+++ project.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
 
-DIST=fedora13
-
-PROJECT_NAME=PFWrapDeformer
-VERSION=1.0.0
-SOURCES=(
-    PFWrapDeformer.cpp
-)
-CFLAGS=(-Wno-deprecated -fPIC -g -Wall) 
-TYPE=library
-APPLICATION=maya
-APPLICATION_VERSION=2011sp1
-OPTIONS=no_prefix
-
-source install_project
-
