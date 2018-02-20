4e0510e7ecf398685adbab3d12718ab4f01328df
Nikolaos Giotis <nyotis@users.noreply.github.com>
2018-02-20 09:44:48 +0100

    Update CustomWrapDeformer.h
    TOY CODE NEEDS RE-WRITING

--- CustomWrapDeformer.h
+++ CustomWrapDeformer.h
@@ -35,39 +35,40 @@ public:
    virtual       ~CustomWrapDeformer();
    static void*   creator();    // whenever attribute needs to be re-evaluated
    static MStatus initialize(); // initialize executes whenever plug-in loads
+
+      // brains of CustomWrapDeformer, it determines all output.
+      // best practices in Maya suggest [1], [2] to implement the deform function and let MPxDeformerNode internally handle the compute function
       
-   virtual MStatus deform( MDataBlock& data, 
-                           MItGeometry& iterGeo, 
-                           const MMatrix& localToWorldMatrix, 
-                           uint geomIndex );
-   // brains of CustomWrapDeformer, it determines all output.
-   // best practices in Maya suggest [1], [2] to implement the deform function and let MPxDeformerNode internally handle the compute function
-      
-   // [1] (david gould)
-   // [2] MAYA API guide
+        // [1] (david gould)
+        // [2] MAYA API guide
+
+   virtual MStatus deform(MDataBlock& data, 
+                          MItGeometry& iterGeo, 
+                          const MMatrix& localToWorldMatrix, 
+                          uint geomIndex);
    
-   // node attribute objects (all static allocated in CustomWrapDeformer.cpp) 
+      // node attribute objects (all static allocated in CustomWrapDeformer.cpp) 
    static MObject input_poly;          // holds a reference to the wrap-base 
    static MObject initialized_data;    // pre-allocates data 
    static MObject distance_multiplier; // introduces the Shrink functionality
    static MObject distance_addition;   // introduces the Expand functionality
    static MObject global_scale;  
    
-   // assign to each vertex of the wrap-base a distance and a direction 
-   // to wrap-influence's closest vertex
+    // assign to each vertex of the wrap-base a distance and a direction 
+    // to wrap-influence's closest vertex
    
-   // length of UV vector
+      // length of UV vector
    static MObject distances; 
-   // normalized vector holding XYZ directions 
-   // towards influence's closest vertex 
+      // normalized vector holding XYZ directions 
+      // towards influence's closest vertex 
    static MObject directions;  
    
    static MObject uv_coordinates; // UV coords at influence's closest vertex
    static MObject normals_xyz;    // XYZ normals vectors at influence's closest
                                   // UV vertex
   
-   // Each node needs to be registered (it requires a unique identifier
-   // within range 0x00000000-0x00000007 -> local use only) 
+      // Each node needs to be registered (it requires a unique identifier
+      // within range 0x00000000-0x00000007 -> local use only) 
    static MTypeId id; 
 
 };
